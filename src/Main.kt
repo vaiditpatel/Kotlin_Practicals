e80c006405931aa9794b47935dde0b1d1d3f18b1
vaiditpatel <vaiditp18@gmail.com>
2026-06-29 10:38:12 +0530

    first push

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val kotlin = "🙂"
+    var i = 1
+    var j : Int = 2
+    var a : Double = 1.5
+    var b : String = "vaidit"
+    val c : Char = 'A'
+    var d : Float = 1.35f
+    var e : Boolean = false
+    var f : Long = -2
+    var g : Byte = 127
+
+    println("i = "+i)
+    println("j = "+j)
+    println("a = "+a)
+    println("b = "+b)
+    println("c = "+c)
+    println("d = "+d)
+    println("e = "+e)
+    println("f = "+f)
+    println("g = "+g)
+    println(kotlin)
+
+}
